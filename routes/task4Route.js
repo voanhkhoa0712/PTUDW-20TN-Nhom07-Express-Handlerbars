f9b75e25930f9ced2d82f0e453b03bcffd8f29e0
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2023-05-11 22:07:16 +0700

    Update task 4

--- routes/task4Route.js
+++ routes/task4Route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const zodiacs = require("../data").zodiacs;
 
 router.get("/", (req, res) => {
-  res.render("task4", { zodiacs: zodiacs, isMenu: true, detail: {} });
+  res.render("task4", { zodiacs });
 });
 
 router.get("/:zodiac", (req, res) => {
@@ -12,7 +12,7 @@ router.get("/:zodiac", (req, res) => {
   const zodiacProp = selectedZodiac.length > 0 ? selectedZodiac[0] : zodiacs[0];
   console.log(zodiacProp);
 
-  res.render("task4", { zodiacs: zodiacs, isMenu: false, detail: zodiacProp });
+  res.render("task4-details", { zodiacs: zodiacs, detail: zodiacProp });
 });
 
 module.exports = router;
